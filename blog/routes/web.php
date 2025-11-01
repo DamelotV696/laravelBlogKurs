8f420029fcf747a0c33e18cec673d09679ce4e58
Damelot <sysoye@inbox.ru>
2025-11-01 11:54:04 +0300

    Laravel создание Блога 2. Первоначальный план и создаем миграции

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
+// Стартовая страница
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Автоматические маршруты авторизации (login, register, logout, password reset)
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Главная страница после логина
+Route::get('/home', [HomeController::class, 'index'])->name('home');
