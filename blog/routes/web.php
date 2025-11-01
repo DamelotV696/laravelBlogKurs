2c13e937d0da22c0b0fdc361c716fd16f4e82f92
Damelot <sysoye@inbox.ru>
2025-11-01 12:33:25 +0300

    Laravel создание Блога 3. Подгружаем фронт страницы блога. Список постов

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,14 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Main\IndexController;
 
-// Стартовая страница
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', IndexController::class);
 
-// Автоматические маршруты авторизации (login, register, logout, password reset)
 Auth::routes();
-
-// Главная страница после логина
-Route::get('/home', [HomeController::class, 'index'])->name('home');
