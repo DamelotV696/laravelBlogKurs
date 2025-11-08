634e190028fb8bed5d3b05b7d8a1b967c98da8e9
Damelot <sysoye@inbox.ru>
2025-11-08 10:28:58 +0300

    Laravel создание Блога 6. Страница создания категории(форма создания)

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Main\indexController;
 use App\Http\Controllers\Admin\Main\AdminIndexController;
 use App\Http\Controllers\Admin\Category\CategoryIndexController;
+use App\Http\Controllers\Admin\Category\CreateIndexController;
 use App\Models\Category;
 
 Route::get('/', indexController::class);
@@ -14,6 +15,7 @@ Auth::routes();
 Route::prefix('admin')->group(function () {
     Route::get('/', AdminIndexController::class);
     Route::prefix('categories')->group(callback: function () {
-        Route::get('/', CategoryIndexController::class);
+        Route::get('/', CategoryIndexController::class)->name('admin.category.index');
+        Route::get('/create', CreateIndexController::class)->name('admin.category.create');
     });
 });
