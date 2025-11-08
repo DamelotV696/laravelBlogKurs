499e902a7cd75e49aee614f75415212ef8e07444
Damelot <sysoye@inbox.ru>
2025-11-08 11:26:05 +0300

    Laravel создание Блога 10. Страница для формы редактирования категории

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Category\CategoryIndexController;
 use App\Http\Controllers\Admin\Category\CreateController;
 use App\Http\Controllers\Admin\Category\StoreController;
 use App\Http\Controllers\Admin\Category\ShowController;
+use App\Http\Controllers\Admin\Category\EditController;
 use App\Models\Category;
 
 Route::get('/', indexController::class);
@@ -21,5 +22,6 @@ Route::prefix('admin')->group(function () {
         Route::get('/create', CreateController::class)->name('admin.category.create');
         Route::post('/', StoreController::class)->name('admin.category.store');
         Route::get('/{category}', ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
     });
 });
