8db78d1be959baf73c6d2c6528d142110302cded
Damelot <sysoye@inbox.ru>
2025-11-08 11:17:32 +0300

    Laravel создание Блога 9. Получение одной категории. Страница категории

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\Main\AdminIndexController;
 use App\Http\Controllers\Admin\Category\CategoryIndexController;
 use App\Http\Controllers\Admin\Category\CreateController;
 use App\Http\Controllers\Admin\Category\StoreController;
+use App\Http\Controllers\Admin\Category\ShowController;
 use App\Models\Category;
 
 Route::get('/', indexController::class);
@@ -19,5 +20,6 @@ Route::prefix('admin')->group(function () {
         Route::get('/', CategoryIndexController::class)->name('admin.category.index');
         Route::get('/create', CreateController::class)->name('admin.category.create');
         Route::post('/', StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', ShowController::class)->name('admin.category.show');
     });
 });
