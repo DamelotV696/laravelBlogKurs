6679d6adfc344aba655a1995fc5d61267c8cea24
Damelot <sysoye@inbox.ru>
2025-11-11 18:21:39 +0300

    Laravel создание Блога 13. Полностью добавляем CRUD для тегов

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,30 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Main\indexController;
+use App\Http\Controllers\Main\IndexController;
 use App\Http\Controllers\Admin\Main\AdminIndexController;
-use App\Http\Controllers\Admin\Category\CategoryIndexController;
-use App\Http\Controllers\Admin\Category\CreateController;
-use App\Http\Controllers\Admin\Category\StoreController;
-use App\Http\Controllers\Admin\Category\ShowController;
-use App\Http\Controllers\Admin\Category\EditController;
-use App\Http\Controllers\Admin\Category\UpdateController;
-use App\Http\Controllers\Admin\Category\DeleteController;
-use App\Models\Category;
 
-Route::get('/', indexController::class);
+Route::get('/', IndexController::class);
 
 Auth::routes();
 
 Route::prefix('admin')->group(function () {
     Route::get('/', AdminIndexController::class);
-    Route::prefix('categories')->group(callback: function () {
-        Route::get('/', CategoryIndexController::class)->name('admin.category.index');
-        Route::get('/create', CreateController::class)->name('admin.category.create');
-        Route::post('/', StoreController::class)->name('admin.category.store');
-        Route::get('/{category}', ShowController::class)->name('admin.category.show');
-        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
-        Route::delete('/{category}', DeleteController::class)->name('admin.category.delete');
-        
+
+    // Категории
+    Route::prefix('categories')->namespace('App\\Http\\Controllers\\Admin\\Category')->group(function () {
+        Route::get('/', 'CategoryIndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
+        Route::post('/', 'StoreController')->name('admin.category.store');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
+        Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
+        Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
+    });
+
+    // Теги
+    Route::prefix('tags')->namespace('App\\Http\\Controllers\\Admin\\Tag')->group(function () {
+        Route::get('/', 'IndexController')->name('admin.tag.index');
+        Route::get('/create', 'CreateController')->name('admin.tag.create');
+        Route::post('/', 'StoreController')->name('admin.tag.store');
+        Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
+        Route::get('/{tag}/edit', 'EditController')->name('admin.tag.edit');
+        Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
     });
 });
